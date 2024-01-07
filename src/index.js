17ab67440764d46bf2de8a4322aab14206700ac2
onkarpawar <onkarpawar25@gmail.com>
2024-01-07 18:24:10 +0530

    utils file added

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log(`ERROR: ${error}`)
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`SERVER IS RUNNING ON PORT: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!!", err)
+})
